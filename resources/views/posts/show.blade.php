0f047d8909f3e6a7419ccde4cbc5b420444a5457
rahmat23 <rahmatullahahmadi500@gmail.com>
2019-04-29 23:31:17 +0200

    some CSS changes on page show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,35 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
     <a href="/posts" class="btn btn-primary">Go back</a>
-    <h1>{{$post->title}}</h1>
-    <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
-
-    <small>Written on {{$post->created_at}}</small>
-    <div>
+    <div class="text-center">
+        <h1>{{$post->title}}</h1>
+    </div>
+    <div class="row">
+    <div class="rounded float-left">
+        <img class="col-4" src="/storage/cover_images/{{$post->cover_image}}">
+    </div>
+    <div class="col">
+        <small>Written on {{$post->created_at}}</small>
+    </div>
+    <div class="col">
         {{$post->auteur}}
     </div>
-    <div>
+    <div class="col">
         {{$post->country}}
     </div>
-    <div>
+    <div class="col">
         {{$post->city}}
     </div>
-    <div>
+    <div class="col">
         {{$post->postalCode}}
     </div>
-    <div>
+    <div class="col">
         {{$post->departure}}
     </div>
-    <div>
+    <div class="col">
         {{$post->arrive}}
     </div>
-
+</div>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
 
 
-    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
         {{Form::hidden('_method', 'DELETE')}}
         {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
     {!!Form::close()!!}
 @endsection
+</div>
